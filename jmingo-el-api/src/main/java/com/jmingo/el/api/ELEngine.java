58a9f269ca6a57d25a235fd4387a9b1cf3ded164
dmgcodevil <dmgcodevil@gmail.com>
2014-06-29 18:13:34 +0300

    change javadoc

--- jmingo-el-api/src/main/java/com/jmingo/el/api/ELEngine.java
+++ jmingo-el-api/src/main/java/com/jmingo/el/api/ELEngine.java
@@ -19,6 +19,7 @@ import java.util.Map;
 
 /**
  * Base interface declares methods to evaluate a expression language.
+ * It's important: implementations must have default constructor.
  */
 public interface ELEngine {
 
